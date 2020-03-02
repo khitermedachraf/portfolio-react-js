7ca5f16092f312d69a9ccd1c55fad2c1a9e13f76
Abderahmane230 <darkvally4@gmail.com>
2020-03-02 17:03:53 +0100

    Updates

---
+++ precache-manifest.d9b637185461804e1a61e496b00fe05b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c45f4f86edd86adab4a7497687956927",
+    "url": "/portfolio-react-js/index.html"
+  },
+  {
+    "revision": "db2952d9ff11e45c4bd7",
+    "url": "/portfolio-react-js/static/css/2.a6bc1c31.chunk.css"
+  },
+  {
+    "revision": "83d3bd8bc6620ba315b6",
+    "url": "/portfolio-react-js/static/css/main.09136764.chunk.css"
+  },
+  {
+    "revision": "db2952d9ff11e45c4bd7",
+    "url": "/portfolio-react-js/static/js/2.6031a9db.chunk.js"
+  },
+  {
+    "revision": "a1e12f97867665430e5c8e39fc9f180a",
+    "url": "/portfolio-react-js/static/js/2.6031a9db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83d3bd8bc6620ba315b6",
+    "url": "/portfolio-react-js/static/js/main.d71c07ae.chunk.js"
+  },
+  {
+    "revision": "83cfd21c1618cfbdcfae",
+    "url": "/portfolio-react-js/static/js/runtime-main.5bcb5c29.js"
+  }
+]);
